15eb5a33caa113b6b0b89546cb2f14745bc00437
LovroD77359 <68549602+LovroD77359@users.noreply.github.com>
2023-12-19 22:08:29 +0100

    Patient Dash
    - elements, functions, style for patient dash
    - temp testing data file to simulate database
    - images and other small adjustments

--- components/PatientDash.jsx
+++ components/PatientDash.jsx
@@ -1,9 +1,15 @@
 import React, { useState } from "react"
-import axios from "axios"
+import axios, { formToJSON } from "axios"
+import { mySchedule } from "./TestingData"
+import map from "../assets/hospital_map1.png"
+import refresh from "../assets/refresh.png"
 import s from "../styles/patientDash.module.css"
 
 export default function PatientDash(props) {
-
+   
+   const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+   const month = ["January","February","March","April","May","June","July","August","September","October","November","December"]
+   
    const [userData, setUserData] = useState({         // state za cuvanje podataka o korisniku
       id: "",
       firstName: "",
@@ -13,38 +19,163 @@ export default function PatientDash(props) {
       active: "",
       role: "",
    })
+   const [selectedWeek, setSelectedWeek] = useState(new Date())
+   const [selectedSession, setSelectedSession] = useState(mySchedule[formatWeek(new Date())][0])
+   const [notesDisabled, setNotesDisabled] = useState(false)
+   const [rescheduleDisabled, setRescheduleDisabled] = useState(true)
+   
+   if (mySchedule[formatWeek(selectedWeek)] != null) {   
+      var scheduleElements = mySchedule[formatWeek(selectedWeek)].map(session => {               // mapiranje podataka iz testingdata na jsx (html) elemente za ispis
+         const { id, datetime, location, sessionNumber, therapist } = session
+         return (
+            <div className={s.session_card} key={id}>
+               <h3 className={s.session_date}>{formatDate(datetime)}</h3>
+               <h3 className={s.session_time}>{formatTime(datetime)}</h3>
+               <p className={s.session_location}>{location}</p>
+               <p className={s.session_more}
+                  onClick={() => setSelectedSession(mySchedule[formatWeek(selectedWeek)][id])}>View more
+               </p>
+            </div>
+         )
+      })
+   } else {
+      var scheduleElements = <p className={s.no_sessions}>No sessions this week.</p>
+   }
 
-   axios({
-      url: "https://medbay-backend-0a5b8fe22926.herokuapp.com/api/user",
-      method: "GET",
-      headers: {
-         Authorization: `Bearer ${props.userToken}`         // korisnikov access token potreban za dohvacanje podataka iz baze
+   function formatWeek(datetime) {
+      let tempDate = new Date(datetime)
+      let date = tempDate.getDate()
+      let weekday = tempDate.getDay()
+      date -= weekday == 0 ? 6 : (weekday - 1)
+      let formattedWeek = date.toString() + addExtension(date) + " " + 
+                          month[tempDate.getMonth()] + " - "
+      tempDate.setDate(date + 6)
+      date = tempDate.getDate()
+      formattedWeek += date.toString() + addExtension(date) + " " +
+                        month[tempDate.getMonth()]
+      return formattedWeek
+   }
+
+   function formatDate(datetime) {
+      let formattedDate = ""
+      formattedDate += weekday[datetime.getDay()] + ", "
+      formattedDate += (datetime.getDate()).toString()
+      formattedDate += addExtension(datetime.getDate()) + " "
+      formattedDate += month[datetime.getMonth()]
+      return formattedDate
+   }
+
+   function addExtension(date) {
+      switch (date) {
+         case 1:
+         case 21:
+         case 31:
+            return "st"
+         case 2:
+         case 22:
+            return "nd"
+         case 3:
+         case 23:
+            return "rd"
+         default:
+            return "th"
       }
-   })
-   .then(res => setUserData({
-      id: res.data.id,
-      firstName: res.data.firstName,
-      lastName: res.data.lastName,
-      email: res.data.email,
-      password: res.data.password,
-      active: res.data.active,
-      role: res.data.role
-   }))
-   .catch(error => console.log(error));
+   }
+
+   function formatTime(datetime) {
+      let formattedTime = datetime.getHours() + ":" + (datetime.getMinutes() ? datetime.getMinutes() : "00") + " - "
+      let tempDate = new Date(datetime.getTime())
+      tempDate.setMinutes(datetime.getMinutes() + 30)
+      formattedTime += tempDate.getHours() + ":" + (tempDate.getMinutes() ? tempDate.getMinutes() : "00")
+      return formattedTime
+   }
+
+   function goBackWeek() {
+      setSelectedWeek(prevDate => {
+         let newDate = new Date(prevDate)
+         newDate.setDate(prevDate.getDate() - 7)
+         return newDate
+      })
+   }
+
+   function goForwardWeek() {
+      setSelectedWeek(prevDate => {
+         let newDate = new Date(prevDate)
+         newDate.setDate(prevDate.getDate() + 7)
+         return newDate
+      })
+   }
+
+   // axios({
+   //    url: "https://medbay-backend-0a5b8fe22926.herokuapp.com/api/user",
+   //    method: "GET",
+   //    headers: {
+   //       Authorization: `Bearer ${props.userToken}`         // korisnikov access token potreban za dohvacanje podataka iz baze
+   //    }
+   // })
+   // .then(res => setUserData({
+   //    id: res.data.id,
+   //    firstName: res.data.firstName,
+   //    lastName: res.data.lastName,
+   //    email: res.data.email,
+   //    password: res.data.password,
+   //    active: res.data.active,
+   //    role: res.data.role
+   // }))
+   // .catch(error => console.log(error));
 
    return (
-      <>
-         <div className={s.patient_main}>
-            <h1>Welcome {userData.firstName}!</h1>
-            <h3>Here is your data:</h3>
-            <p>Id: {userData.id}</p>
-            <p>First name: {userData.firstName}</p>
-            <p>Last name: {userData.lastName}</p>
-            <p>Email: {userData.email}</p>
-            <p>Password (encrypted): {userData.password}</p>
-            <p>Activity status: {userData.active ? "true" : "false"}</p>
-            <p>Role: {userData.role}</p>
+      <div className={s.patient_dash_main}>
+         <div className={s.container_left}>
+            <h2 className={s.container_title}>My schedule:</h2>
+
+            <div className={s.date_wrapper}>
+               <span className={s.date_arrow} onClick={goBackWeek}>&#60;</span>
+               <p className={s.container_date}>{formatWeek(selectedWeek)}</p>
+               <span className={s.date_arrow} onClick={goForwardWeek}>&#62;</span>
+            </div>
+            <img src={refresh} className={s.date_reset} onClick={() => setSelectedWeek(new Date())} />
+            
+            <div className={s.scroll_container}>
+               <div className={s.schedule_card}>
+                  {scheduleElements}
+               </div>
+            </div>
          </div>
-      </>
+
+         <div className={s.container_right}>
+            <h2 className={s.container_title}>Next session:</h2>
+            <p className={s.container_date}>{formatDate(selectedSession.datetime)}</p>
+            
+            <div className={s.selected_session}>
+               <div className={s.session_info}>
+                  <div className={s.info_labels}>
+                     <p>Time:</p>
+                     <p>Location:</p>
+                     <p>Session number:</p>
+                     <p>Therapist:</p>
+                  </div>
+                  <div className={s.info_values}>
+                     <p>{formatTime(selectedSession.datetime)}</p>
+                     <p>{selectedSession.location}</p>
+                     <p>{selectedSession.sessionNumber}</p>
+                     <p>{selectedSession.therapist}</p>
+                  </div>
+               </div>
+
+               <img src={map} className={s.session_image} />
+
+               <div className={s.session_buttons}>
+                  <button className={`${s.session_button} ${notesDisabled && s.button_disabled}`}>View notes</button>
+                  <button className={`${s.session_button} ${rescheduleDisabled && s.button_disabled}`}>Reschedule</button>
+                  
+                  <p className={`${s.notes_text} ${notesDisabled && s.disabled_text}`}>
+                     No notes so far.<br />­</p>                           {/* iza br sam ubacio ALT + 0173 za poravnanje */}
+                  <p className={`${s.reschedule_text} ${rescheduleDisabled && s.disabled_text}`}>
+                     Cannot reschedule.<br />Appointment is in less than 48 hours.</p>
+               </div>
+            </div>
+         </div>
+      </div>
    )
 }
---
+++ components/TestingData.jsx
@@ -0,0 +1,62 @@
+export const mySchedule = {
+   "18th December - 24th December": [
+      {
+         id: 0,
+         datetime: new Date ("2023-12-18 11:30"),
+         location: "Electrotherapy room",
+         sessionNumber: "4/11",
+         therapist: "Ante Pavlović"
+      },
+      {
+         id: 1,
+         datetime: new Date ("2023-12-19 11:30"),
+         location: "Electrotherapy room",
+         sessionNumber: "5/11",
+         therapist: "Ante Pavlović"
+      },
+      {
+         id: 2,
+         datetime: new Date ("2023-12-20 11:30"),
+         location: "Electrotherapy room",
+         sessionNumber: "6/11",
+         therapist: "Ante Pavlović"
+      },
+      {
+         id: 3,
+         datetime: new Date ("2023-12-21 11:30"),
+         location: "Electrotherapy room",
+         sessionNumber: "7/11",
+         therapist: "Ante Pavlović"
+      },
+      {
+         id: 4,
+         datetime: new Date ("2023-12-22 11:30"),
+         location: "Electrotherapy room",
+         sessionNumber: "8/11",
+         therapist: "Ante Pavlović"
+      },
+      {
+         id: 5,
+         datetime: new Date ("2023-12-23 11:30"),
+         location: "Electrotherapy room",
+         sessionNumber: "9/11",
+         therapist: "Ante Pavlović"
+      }
+   ],
+   "25th December - 31st December": [
+      {
+         id: 0,
+         datetime: new Date ("2023-12-26 11:30"),
+         location: "Electrotherapy room",
+         sessionNumber: "10/11",
+         therapist: "Ante Pavlović"
+      },
+      {
+         id: 1,
+         datetime: new Date ("2023-12-29 11:30"),
+         location: "Electrotherapy room",
+         sessionNumber: "11/11",
+         therapist: "Ante Pavlović"
+      }
+   ]
+}
